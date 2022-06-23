5e70354096fa74c4bea7402a8ffc6e495fe8a35a
Zr0AM <103156517+Zr0AM@users.noreply.github.com>
2022-06-23 18:12:52 -0500

    Another change test

--- www/index.php
+++ www/index.php
@@ -6,7 +6,7 @@ require_once 'html/htmlwrapper.php';
 $objHtml = new html\htmlwrapper();
 $objHtml::writeHeader($_SERVER["PHP_SELF"]);
 
-echo '<span> Welcome to Team Omnomnom\'s Tasks application. </span><br> <br>';
+echo '<span> Welcome to Team Omnomnom\'s Tasks application.</span><br> <br>';
 
 /*
  * echo print_r($_SESSION);
